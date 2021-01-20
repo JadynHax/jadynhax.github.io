d1fa4b5a519095d2222d415489eac96b607e0efa
JaonHax <jaonhax@gmail.com>
2021-01-20 15:02:48 -0600

    Update common.js

--- assets/common.js
+++ assets/common.js
@@ -7,15 +7,16 @@ function htmlDecode(text) {
 }
 
 document.onload = function() {
-    document.body.innerHTML += `
-        <div id="source-view-bkg">
-            <span id="close-source">&times;</span>
-            <h1>Page Source</h1>
-            <pre id="source-view"><code>
-                <!-- Page source gets loaded here -->
-            </code></pre>
-        </div>
-        <button id="view-source"><i class="fas fa-file-code"></i> View Source</button>`;
+    document.body.appendChild(new DOMParser().parseFromString(`<div>
+            <div id="source-view-bkg">
+                <span id="close-source">&times;</span>
+                <h1>Page Source</h1>
+                <pre id="source-view"><code>
+                    <!-- Page source gets loaded here -->
+                </code></pre>
+            </div>
+            <button id="view-source"><i class="fas fa-file-code"></i> View Source</button>
+        </div>`, "text/html"));
     $("#source-view > code").load(window.location.pathname, function(){$("#source-view > code").text($("#source-view > code").html());hljs.highlightAuto(document.querySelector("#source-view > code"))})
 
     document.querySelector("#view-source").addEventListener("click", function(){document.querySelector("#source-view-bkg").style.opacity = 1; document.querySelector("#source-view-bkg").style.pointerEvents = "auto"})
