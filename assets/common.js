8c7b286202ec62efc93d2b9b486fc4553b2c17bf
JaonHax <jaonhax@gmail.com>
2021-01-20 15:07:05 -0600

    Update common.js

--- assets/common.js
+++ assets/common.js
@@ -6,7 +6,9 @@ function htmlDecode(text) {
   return $('<div/>').html(text).text();
 }
 
-document.body.appendChild(new DOMParser().parseFromString(`<div>
+temp = document.createElement("div")
+temp.innerHTML = `
+        <div>
             <div id="source-view-bkg">
                 <span id="close-source">&times;</span>
                 <h1>Page Source</h1>
@@ -15,7 +17,9 @@ document.body.appendChild(new DOMParser().parseFromString(`<div>
                 </code></pre>
             </div>
             <button id="view-source"><i class="fas fa-file-code"></i> View Source</button>
-        </div>`, "text/html"));
+        </div>`
+
+document.body.appendChild(temp.firstChild, "text/html"));
 $("#source-view > code").load(window.location.pathname, function(){$("#source-view > code").text($("#source-view > code").html());hljs.highlightAuto(document.querySelector("#source-view > code"))})
 
 document.querySelector("#view-source").addEventListener("click", function(){document.querySelector("#source-view-bkg").style.opacity = 1; document.querySelector("#source-view-bkg").style.pointerEvents = "auto"})
