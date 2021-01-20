f84cd6180a3f4e6aa3babcf15480c149c8432ea9
JaonHax <jaonhax@gmail.com>
2021-01-20 15:04:41 -0600

    Update common.js

--- assets/common.js
+++ assets/common.js
@@ -6,8 +6,8 @@ function htmlDecode(text) {
   return $('<div/>').html(text).text();
 }
 
-document.onload = function() {
-    document.body.appendChild(new DOMParser().parseFromString(`<div>
+
+document.body.appendChild(new DOMParser().parseFromString(`<div>
             <div id="source-view-bkg">
                 <span id="close-source">&times;</span>
                 <h1>Page Source</h1>
@@ -17,8 +17,7 @@ document.onload = function() {
             </div>
             <button id="view-source"><i class="fas fa-file-code"></i> View Source</button>
         </div>`, "text/html"));
-    $("#source-view > code").load(window.location.pathname, function(){$("#source-view > code").text($("#source-view > code").html());hljs.highlightAuto(document.querySelector("#source-view > code"))})
+$("#source-view > code").load(window.location.pathname, function(){$("#source-view > code").text($("#source-view > code").html());hljs.highlightAuto(document.querySelector("#source-view > code"))})
 
-    document.querySelector("#view-source").addEventListener("click", function(){document.querySelector("#source-view-bkg").style.opacity = 1; document.querySelector("#source-view-bkg").style.pointerEvents = "auto"})
-    document.querySelector("#close-source").addEventListener("click", function(){document.querySelector("#source-view-bkg").style.opacity = 0; document.querySelector("#source-view-bkg").style.pointerEvents = "none"})
-}
+document.querySelector("#view-source").addEventListener("click", function(){document.querySelector("#source-view-bkg").style.opacity = 1; document.querySelector("#source-view-bkg").style.pointerEvents = "auto"})
+document.querySelector("#close-source").addEventListener("click", function(){document.querySelector("#source-view-bkg").style.opacity = 0; document.querySelector("#source-view-bkg").style.pointerEvents = "none"})
