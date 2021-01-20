fe6505b7e901a5d417b4e52dc6ae452de418a4e3
JaonHax <jaonhax@gmail.com>
2021-01-20 16:36:42 -0600

    Update common.js

--- assets/common.js
+++ assets/common.js
@@ -13,7 +13,7 @@ window.onload = function() {
                 <span id="close-source">&times;</span>
                 <h1>Page Source</h1>
                 <div>
-                    <pre><code>
+                    <pre class="hljs html"><code>
                         <!-- Page source gets loaded here -->
                     </code></pre>
                 </div>
@@ -27,6 +27,5 @@ window.onload = function() {
 
     document.querySelector("#view-source").onclick = function(){document.querySelector("#source-view-bkg").style.opacity = 1; document.querySelector("#source-view-bkg").style.pointerEvents = "auto"}
     document.querySelector("#close-source").onclick = function(){document.querySelector("#source-view-bkg").style.opacity = 0; document.querySelector("#source-view-bkg").style.pointerEvents = "none"}
-    hljs.highlightBlock(document.querySelector("#source-view > div > pre"));
     window.onload = undefined
 };
