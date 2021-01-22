a41ac9adf470b2f563b29553749ef9cf074cc524
JaonHax <jaonhax@gmail.com>
2021-01-22 10:24:31 -0600

    Update obfuscator.js

--- assets/obfuscator.js
+++ assets/obfuscator.js
@@ -26,21 +26,46 @@ class Obfuscator {
         params.dispTime = params.dispTime || params.disp_time;
         params.chars = params.chars || params.obfu_chars;
 
+        // Get only the parameters we need, as well as setting defaults
+        var {
+            delay = 0,
+            startTime = 40,
+            endTime = 60,
+            dispTime = 1750,
+            loop = false,
+            chars = "0123456789!<>-_\\/[]{}—=+*^?#",
+            phrases,
+        } = params
+
+        // Properly escape obfuChars and get the characters in an array
+        obfuChars = this.getCharArray(obfuChars.replace("<", "&lt;").replace(">", "&gt;"));
+
         // Actual class attribute assignments
-        this.el = el;
-        this.counter = 0
-        this.params = params;
+        this.el = typeof el == "object" ? el : document.querySelector(el);
+        this.counter = 0;
+        this.params = {
+            "delay": delay,
+            "startTime": startTime,
+            "endTime": endTime,
+            "dispTime": dispTime,
+            "loop": loop,
+            "chars": chars,
+            "phrases": phrases,
+        };
         this.update = this.update.bind(this);
     }
     obfuscate() {
         this.counter = 0
         const displayNext = () => {
+            // Transition to the next phrase and set up the next displayNext call, or stop displaying if done.
             if (0 <= this.counter < this.params.phrases.length) {
                 this.setText(this.params.phrases[this.counter]).then(() => {
-                    setTimeout(displayNext, this.params.dispTime)
+                    setTimeout(displayNext, this.params.dispTime);
                 })
             }
+            // Increment counter
             this.counter++;
+            // Make sure counter is mod phrases.length if looping
             if (this.params.loop) {
                 this.counter %= this.params.phrases.length;
             }
@@ -48,56 +73,81 @@ class Obfuscator {
         setTimeout(displayNext, this.params.delay);
     }
     setText(newText) {
-        const oldText = this.el.innerHTML
-        const length = Math.max(oldText.length, newText.length)
-        const promise = new Promise((resolve) => this.resolve = resolve)
-        this.queue = []
+        newText = this.getCharArray(newText);
+        const oldText = this.getCharArray(this.el.innerHTML);
+        const length = Math.max(oldText.length, newText.length);
+        // Use a Promise for ability to use post-animation operations
+        const promise = new Promise((resolve) => this.resolve = resolve);
+        // Append chars to a queue able to be used by the .update() method
+        this.queue = [];
         for (let i = 0; i < length; i++) {
-            const from = oldText[i] || ''
-            const to = newText[i] || ''
-            const start = Math.floor(Math.random() * this.startTime)
-            const end = start + Math.floor(Math.random() * this.endTime)
-            this.queue.push({ from, to, start, end })
+            // Handle strings with differing lengths
+            const from = oldText[i] || '';
+            const to = newText[i] || '';
+            // Determine a good random start time
+            const start = Math.floor(Math.random() * this.params.startTime);
+            const end = start + Math.floor(Math.random() * this.params.endTime);
+            this.queue.push({ from, to, start, end });
         }
-        cancelAnimationFrame(this.frameRequest)
-        this.frame = 0
-        this.update()
-        return promise
+        // Make sure old animation is canceled so we can start a new one
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        // Again, that post-animation operation functionality
+        return promise;
+    }
+    getCharArray(string) {
+        var charArray = [];
+        if (string) {
+            // Treat HTML entities, tags, and symbols as a single character.
+            const charPattern = /(<.+?>)|(\&[a-z]+?;)|(\&#[0-9]+?;)|(\&#x[0-9a-f]+?;)|./gi
+            // Get match first so we can push to charArray as the first part of the loop.
+            var match = charPattern.exec(string);
+            do {
+                charArray.push(match[0]);
+                // Get next match so we can make sure it isn't null before we loop.
+                match = charPattern.exec(string);
+            } while (match != null);
+        }
+        return charArray
     }
     update() {
-        let output = ''
-        let complete = 0
-        for (let i = 0, n = this.queue.length; i < n; i++) {
-            let { from, to, start, end, char } = this.queue[i]
+        let output = '';
+        let complete = 0;
+        // Loop through the character queue, obfuscating each character along the way.
+        for (let i = 0; i < this.queue.length; i++) {
+            let { from, to, start, end } = this.queue[i];
             if (this.frame >= end) {
-                complete++
-                output += to
+                // Count completed characters
+                complete++;
+                output += to;
             } else if (this.frame >= start) {
-                if (!char || Math.random() < 0.28) {
-                    char = this.randomChar()
-                    this.queue[i].char = char
-                }
-                output += `<span>${char}</span>`
+                // Make a chance to sub in to or from characters
+                output += `<span>` + ((Math.random() < 0.1) ? ((this.frame >= (end + start) / 2) ? to : from) : this.randomChar()) + `</span>`;
             } else {
-                output += from
+                // Stay static until start frame has passed
+                output += from;
             }
         }
-        this.el.innerHTML = output
+        // Display output in element
+        this.el.innerHTML = output;
         if (complete === this.queue.length) {
-            this.resolve()
+            // Resolve the Promise, allowing for post-animation operations
+            this.resolve();
         } else {
-            this.frameRequest = requestAnimationFrame(this.update)
-            this.frame++
+            // Request the next animation frame when done if still going.
+            this.frameRequest = requestAnimationFrame(this.update);
+            this.frame++;
         }
     }
     randomChar() {
-        return this.params.chars[Math.floor(Math.random() * this.params.chars.length)]
+        return this.params.chars[Math.floor(Math.random() * this.params.chars.length)];
     }
 }
 
 function obfuscateElement(selector, obfuParams) {
-    const el = document.querySelector(selector)
-    const obfu = new Obfuscator(el, obfuParams)
+    const el = document.querySelector(selector);
+    const obfu = new Obfuscator(el, obfuParams);
 
-    obfu.obfuscate()
+    obfu.obfuscate();
 }
