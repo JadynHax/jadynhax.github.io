8ae1d4b77944784bf044b064e934599974b7375d
JaonHax <jaonhax@gmail.com>
2021-02-28 11:53:46 -0600

    Remove filesystem stuff from common.js
    Moving it to its own file

--- assets/js/common.js
+++ assets/js/common.js
@@ -54,29 +54,3 @@ $(function(){
         timing += timeBetween;
     });
 });
-
-
-// Finish this later
-
-const fs = {
-    ls: function(path) {
-        if (path.endsWith("/")) {
-            path = path.slice(0, -1);
-        }
-
-        return $.getJSON(path+"/index.json");
-    },
-    listdir: this.ls,
-    read: function(path) {
-        return $.get(path).text();
-    },
-    getData: function(path) {
-        var data = {};
-
-        if (path.endsWith(".html")) {
-            var content = this.read(path);
-            
-            /<title>(.*?)<\/title>/i
-        }
-    }
-}
